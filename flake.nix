7100d775318ae5b647e070c0509b6e21fe07ac93
lxb <liuxiaobo6663233@gmail.com>
2023-05-28 09:59:38 +0800

    Add flake.nix about ruby.

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A Nix-flake-based React Native development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+
+    utils.lib.eachDefaultSystem (system:
+      let
+        # react-native dependencies versions
+        nodejsVersion = 18;
+
+        overlays = [
+          (final: prev: rec {
+            nodejs = prev."nodejs-${toString nodejsVersion}_x";
+            yarn = prev.yarn.override { inherit nodejs; };
+            ruby = prev.ruby.withPackages (p: [ p.cocoapods ]);
+          })
+        ];
+
+        pkgs = import nixpkgs { inherit overlays system; };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # nodejs 18 (specified by overlay)
+            nodejs
+
+            # yarn with nodejs 18 (specified by overlay)
+            yarn
+
+            # ruby with cocoapods (specified by overlay)
+            ruby
+
+            # react-native development dependencies 
+            watchman
+          ];
+
+
+          shellHook = '''';
+        };
+      });
+}

3262e1c17eca6576c67a953983c2fa4d81303862
lxb <liuxiaobo6663233@gmail.com>
2023-05-28 13:33:21 +0800

    Update flake.nix

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ stdenv, ruby, bundlerEnv, lib, baseurl ? null }:
+
+let
+  gems = bundlerEnv {
+    name = "gelos-site-env";
+    inherit ruby;
+    gemdir = ./.;
+  };
+in
+stdenv.mkDerivation {
+  name = "gelos-site";
+  src = ./.;
+
+  JEKYLL_ENV = "production";
+
+  buildInputs = [ gems ruby ];
+
+  buildPhase = ''
+    ${gems}/bin/bundle exec jekyll build ${lib.optionalString (baseurl != null) "--baseurl ${baseurl}"}
+  '';
+  installPhase = ''
+    mkdir -p $out
+    cp -Tr _site $out/public
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -18,14 +18,14 @@
           (final: prev: rec {
             nodejs = prev."nodejs-${toString nodejsVersion}_x";
             yarn = prev.yarn.override { inherit nodejs; };
-            ruby = prev.ruby.withPackages (p: [ p.cocoapods ]);
+            ruby = prev.ruby_3_2;
           })
         ];
 
         pkgs = import nixpkgs { inherit overlays system; };
 
       in
-      {
+      rec {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             # nodejs 18 (specified by overlay)
@@ -39,10 +39,37 @@
 
             # react-native development dependencies 
             watchman
+
+            bundix
+
+            jekyll
           ];
 
-
-          shellHook = '''';
+          shellHook = ''
+            alias start="bundle exec jekyll serve"
+          '';
+          
         };
+
+      packages = rec {
+        gelos-site = pkgs.callPackage ./default.nix {};
+        gelos-site-serve = pkgs.writeShellScriptBin "serve" ''
+          echo "Serving on http://localhost:8000"
+          ${pkgs.webfs}/bin/webfsd -F -f index.html -r ${gelos-site}/public
+        '';
+        remove-nbsp = pkgs.writeShellScriptBin "remove-nbsp" ''
+          ${pkgs.gnused}/bin/sed 's/\xC2\xA0/ /g' -i $(find . -name '*.md')
+        '';
+        default = gelos-site;
+      };
+
+      apps = rec {
+        gelos-site-serve = {
+          type = "app";
+          program = "${packages.gelos-site-serve}/bin/serve";
+        };
+        default = gelos-site-serve;
+      };
+
       });
 }
